cfdb17f52824ab6b1d69a9fa1197c87b416a2b12
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-05 23:22:08 +0300

    Enemy.cs updated with reference to Rigidbody and Player; AddForce() method added.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 1.0f;
+
+    private GameObject player;
+    private Rigidbody enemyRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
     }
 }
