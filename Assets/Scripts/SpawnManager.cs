ef092fd93824769e4e97b60ddfba12cd8084163a
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-06 00:07:29 +0300

    The spawn position is randomized by Random.Range() method in SpawnManager.cs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        float randomPosX = Random.Range(-spawnRange, spawnRange);
+        float randomPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 spawnPos = new Vector3(randomPosX, 0, randomPosZ);
+
+        Instantiate(enemyPrefab, spawnPos, enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
