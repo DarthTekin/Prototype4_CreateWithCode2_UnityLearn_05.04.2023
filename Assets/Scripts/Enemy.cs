6fb44b74bbb8ad1bfc2993ac921fa3fc592c5069
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-06 23:57:16 +0300

    PlayerController.cs updated by AddForce() method for Enemy; Enemy.cs updated by speed of collision.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float speed = 2.0f;
 
     private GameObject player;
     private Rigidbody enemyRb;
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 1.0f;
     public bool hasPowerup;
 
+    private float powerupStrength = 15.0f;
+
     private GameObject focalPoint;
 
     private Rigidbody playerRb;
@@ -37,6 +39,10 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
             Debug.Log("Collided with" + collision.gameObject.name + "with powerup set to" + hasPowerup);
         }
     }
