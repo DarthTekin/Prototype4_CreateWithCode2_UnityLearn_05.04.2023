e0215cdfbc1b6f80706c00d8bdda88f1f0c96608
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-09 11:51:57 +0300

    RocketBehaviour.cs and corresponding variables added.

---
+++ Assets/Scripts/RocketBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBehaviour : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private float rocketStrength = 15.0f;
+    private float aliveTime = 5.0f;
+    private bool homing;
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
