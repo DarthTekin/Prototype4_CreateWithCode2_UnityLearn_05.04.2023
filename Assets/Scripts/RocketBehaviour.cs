af81c02c64346a4f2cd0d8f84bd2eafffbbb527e
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-09 12:10:50 +0300

    RocketBehaviour.cs updated by OnCollisionEnter() method.

--- Assets/Scripts/RocketBehaviour.cs
+++ Assets/Scripts/RocketBehaviour.cs
@@ -14,13 +14,32 @@ public class RocketBehaviour : MonoBehaviour
         // Update is called once per frame
     void Update()
     {
-        
+        if (homing && target != null)
+        {
+            Vector3 moveDirection = (target.transform.position - transform.position).normalized;
+            transform.position += moveDirection * speed * Time.deltaTime;
+            transform.LookAt(target);
+        }
     }
 
     public void Fire(Transform newTarget)
     {
-        target = homingTarget;
+        target = newTarget;
         homing = true;
         Destroy(gameObject, aliveTimer);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (target != null)
+        {
+            if (collision.gameObject.CompareTag(target.tag))
+            {
+                Rigidbody targetRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+                Vector3 away = -collision.contacts[0].normal;
+                targetRigidbody.AddForce(away * rocketStrength, ForceMode.Impulse);
+                Destroy(gameObject);
+            }
+        }
     }
 }
